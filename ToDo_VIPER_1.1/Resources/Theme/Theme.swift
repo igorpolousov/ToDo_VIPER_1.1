2ebd3099cdd3bc9b2362c0f9f560824f00273cf4
Igor Polousov <igorpolousov@gmail.com>
2025-11-16 17:20:44 +0300

    + readme section 2 update + extessions for color and font in SwiftUI

---
+++ ToDo_VIPER_1.1/Resources/Theme/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Theme.swift
+//  ToDo_VIPER_1.1
+//
+//  Created by Igor Polousov on 16.11.2025.
+//
+
+import SwiftUI
+
+extension Font {
+    public static let toDoHeaderFont: Font = Font.custom("SF-pro", size: 34)
+    public static let toDoMainFont: Font = Font.custom("SF-pro", size: 16)
+    public static let toDoSecondaryFont: Font = Font.custom("SFProText", size: 12)
+}
+
+extension Color {
+    public static let toDoAccentColor: Color = Color("AccentColor")
+    public static let toDoGrayColor: Color = Color("TaskCircleColor")
+}
