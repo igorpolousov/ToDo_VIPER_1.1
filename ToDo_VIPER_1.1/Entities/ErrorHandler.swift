ae8b13963eb1586cd76ae300e2f4425fa4ef4c4a
Igor Polousov <igorpolousov@gmail.com>
2025-11-21 14:19:36 +0300

    + update readme + file ErrorHandler.swift with enum ErrorHandler for providing errors description

---
+++ ToDo_VIPER_1.1/Entities/ErrorHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ErrorHandler.swift
+//  ToDo_VIPER_1.1
+//
+//  Created by Igor Polousov on 21.11.2025.
+//
+
+import Foundation
+
+enum ErrorHandler: Error {
+    // Ошибки получения данных по сети
+    case missingData
+}
+
+extension ErrorHandler: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return " Missing valid data/Отсутствуют необходимые данные"
+        }
+    }
+    
+}
